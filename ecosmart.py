35029eaf8a548d4da9f12a9de4d7b197ec3939d9
Isidora Martínez <isidora.mb2005@gmail.com>
2025-06-09 00:43:35 -0400

    ecosmart.py
    agregar lineas de : 
    from Backend.Agricultor.Monitoreo_de_Cultivos.Visualizacion_datos.visualizacion_graficos import visualizacion_g_blueprint
    app.register_blueprint(visualizacion_g_blueprint)

--- ecosmart.py
+++ ecosmart.py
@@ -40,7 +40,7 @@ from Backend.Agricultor.Monitoreo_de_Cultivos.Visualizacion_datos.visualizacion_
 from Backend.Agronomo.Asistente_IA.recomendacion import sensores_bp
 from Backend.Tecnico.Configuracion_de_alertas.configurar_alerta import configurar_umbrales_alerta_blueprint
 from Backend.Tecnico.Configuracion_de_alertas.notificaciones_app import notificaciones_blueprint
-
+from Backend.Agricultor.Monitoreo_de_Cultivos.Visualizacion_datos.visualizacion_graficos import visualizacion_g_blueprint
 
 # Registrar todos los blueprints
 app.register_blueprint(auth_blueprint)
@@ -58,7 +58,7 @@ app.register_blueprint(visualizacion_g_blueprint)
 app.register_blueprint(sensores_bp)
 app.register_blueprint(configurar_umbrales_alerta_blueprint)
 app.register_blueprint(notificaciones_blueprint)
-
+app.register_blueprint(visualizacion_g_blueprint)
 
 # ----------------------------------------
 # Funciones para manejar el subproceso
